cfce4d322bf03d820935a1ac69a3bded9eb42c4c
Shivam Pandey <shivampandey12345678910@gmail.com>
2025-12-10 01:16:27 +0530

    added production url

--- Client/src/api.js
+++ Client/src/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000',
+  baseURL: 'https://project-management-system-ekhu.onrender.com', 
+  // baseURL: 'http://localhost:5000',
 });
 
 // Add token to requests
